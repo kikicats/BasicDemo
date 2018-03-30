25c8fa1e98aac185c100b00f9ae2948526aaffb9
yuki <yukiyuki_nana@163.com>
2018-03-30 14:48:55 +0800

    the first time commit

---
+++ src/main/java/webdriver2.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class webdriver2 {
+
+    public static void main(String[] args)
+    {
+        WebDriver driver ;
+        //driver = new ChromeDriver();
+        driver = new FirefoxDriver();
+        //driver.get("http://www.baidu.com");
+        driver.navigate().to("http://www.bilibli.com");
+    }
+}
